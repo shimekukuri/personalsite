fcff5a4e5efac3b8a9e8ae20bd405492c972739b
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-07-28 01:36:25 -0400

    added snapping

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Container from "./component/Container";
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Container classes="first-container">
-        <h1 style={{margin: "0px"}}>test</h1>
-      </Container>
-      <Container>
-        <h2>test 2</h2>
-      </Container>
-      <Container><h2>test 3</h2></Container>
+      <div className="snap">
+        <Container classes="first-container">
+          <h1 style={{marginLeft: "8px"}}>test</h1>
+        </Container>
+        <Container>
+          <h2 style={{marginLeft: "8px"}}>test 2</h2>
+        </Container>
+        <Container>
+          <h2 style={{marginLeft: "8px"}}>test 3</h2>
+        </Container>
+      </div>
     </>
   );
 }
--- src/component/Container.js
+++ src/component/Container.js
@@ -3,17 +3,17 @@ import "./Container.css";
 import clsx from "clsx";
 
 export default function Container(props) {
-  const { children, classes, ...rest } = props;
+  const { children, classes, innerClasses, ...rest } = props;
   const classForStyles = clsx("flex-center", [classes].includes("first-container") ? "first-container" : "container");
-
-  //console.log(classForStyles);
+  const innerContainerStyles = clsx("inner-container","border-radi", innerClasses ?? "")
+ 
 
   return (
     <>
       <div
         className={classForStyles}
       >
-        <div className="inner-container">{children}</div>
+        <div className={innerContainerStyles}>{children}</div>
       </div>
     </>
   );
